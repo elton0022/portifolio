834bc78fc855759e2be037d3dccde0589fd95a4b
elton0022 <eltonrodrigues0022.er@gmail.com>
2022-09-12 09:33:12 -0300

    fix: animation and cards

--- src/components/project/index.js
+++ src/components/project/index.js
@@ -5,7 +5,7 @@ import './styles.css';
 
 class Project extends React.Component {
 
-  
+
   openInNewTab(url) {
     window.open(url, '_blank');
   }
@@ -13,15 +13,17 @@ class Project extends React.Component {
   render() {
     return (
       <div className="project" onClick={() => this.openInNewTab(this.props.url)}>
-        <div className="logo">
-          <img className="img" src={require(`../../images/svg/${this.props?.language ? this.props.language.toLowerCase()+'.svg' : 'css.svg'}`)} alt="icon" />
-          <span className="repo-name">
-            {this.props.name}
-          </span>
+        <div className="container-card">
+          <img className="img" src={require(`../../images/svg/${this.props?.language ? this.props.language.toLowerCase() + '.svg' : 'css.svg'}`)} alt="icon" />
+          <div className="repo-content">
+            <span className="repo-name">
+              {this.props.name}
+            </span>
+            <p className="repo-description">
+              {this.props.description}
+            </p>
+          </div>
         </div>
-        <p className="repo-description">
-          {this.props.description}
-        </p>
       </div>
     )
   }
